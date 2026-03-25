4f38e30c9224c001cd74cc913fae0fb41818f322
ymid0610 <ymid0610@gmail.com>
2026-03-25 22:45:38 +0900

    파란 사각형 색입히기

--- LabProjects/LabProject05-01/Shaders.hlsl
+++ LabProjects/LabProject05-01/Shaders.hlsl
@@ -17,18 +17,26 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
     return (output);
 }
 
-#define FRAME_BUFFER_WIDTH 640.0f
-#define FRAME_BUFFER_HEIGHT 480.0f
+#define FRAME_BUFFER_WIDTH 800.0f
+#define FRAME_BUFFER_HEIGHT 600.0f
 
-//픽셀 쉐이더를 다음과 같이 정의하여 실행해 보면 다음과 같은 결과를 볼 수 있다.
+float Rectangle(float2 f2NDC, float fLeft, float fRight, float fTop, float fBottom)
+{
+    float2 f2Shape = float2(step(fLeft, f2NDC.x), step(f2NDC.x, fRight));
+    f2Shape *= float2(step(fTop, f2NDC.y), step(f2NDC.y, fBottom));
+    return (f2Shape.x * f2Shape.y);
+}
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f;
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f; // (0, 1) : (-0.5, 0.5)
     f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-    float fLength = length(f2NDC);
-    float fMin = 0.3f, fMax = 0.2f;
-    cColor.rgb = smoothstep(fMin, fMax, fLength);
+    float2 f2Horizontal = float2(0.1f, +0.3f); //(Left, Right)
+    float2 f2Vertical = float2(-0.3f, +0.3f); //(Top, Bottom)
+    cColor.b = Rectangle(f2NDC, +0.1f, +0.3f, -0.2f, +0.4f);
+    cColor.b += Rectangle(f2NDC, -0.3f, -0.1f, -0.4f, -0.1f);
     return (cColor);
 }
+
+
 

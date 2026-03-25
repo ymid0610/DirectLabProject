22be9cfc016a02b9ec126ca9dce369f69169c51e
ymid0610 <ymid0610@gmail.com>
2026-03-25 22:08:19 +0900

    smoothstep 이용하여 부드럽게 색 입히기

--- LabProjects/LabProject05-01/Shaders.hlsl
+++ LabProjects/LabProject05-01/Shaders.hlsl
@@ -24,10 +24,11 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -0.5f; // (0, 1) : (-0.5, 0.5)
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f;
     f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-    // cColor.b = step(length(f2NDC), 0.25f); //step(x, y) = (x <= y) ? 1 : 0
-    cColor.b = (length(f2NDC) <= 0.25f) ? 1.0f : 0.0f; //step(x, y) = (x <= y) ? 1 : 0
+    float fLength = length(f2NDC);
+    float fMin = 0.3f, fMax = 0.2f;
+    cColor.rgb = smoothstep(fMin, fMax, fLength);
     return (cColor);
 }
 

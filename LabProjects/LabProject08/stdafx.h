4ec1eab549a950400936c80ebe5e8e1c3158e122
ymid0610 <ymid0610@gmail.com>
2026-05-18 21:57:23 +0900

    클래스 Mesh의 자식으로 직육면체 메쉬 추가, Scene 직육면체 그리도록 수정 -> 아무것도 보이지않음 : 카메라가 직육면체 안

--- LabProjects/LabProject08/stdafx.h
+++ LabProjects/LabProject08/stdafx.h
@@ -262,3 +262,7 @@ namespace Matrix4x4
 		return(xmmtx4x4Result);
 	}
 }
+
+/*정점의 색상을 무작위로(Random) 설정하기 위해 사용한다. 각 정점의 색상은 난수(Random Number)를 생성하여
+지정한다.*/
+#define RANDOM_COLOR XMFLOAT4(rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX))
